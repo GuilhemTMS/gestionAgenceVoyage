bec61d3494bc62ddb5b4b497c18cd91f816fb9b8
root <root@localhost.localdomain>
2022-06-01 14:53:11 +0200

    creation du projet

---
+++ src/test/java/com/inti/gestionAgenceVoyage/GestionAgenceVoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inti.gestionAgenceVoyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAgenceVoyageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
