678aaa2f13a7c7940376fba03762d2590881298c
root <root@localhost.localdomain>
2022-06-01 15:11:08 +0200

    Ajout attributs Destination

--- src/main/java/com/inti/model/Destination.java
+++ src/main/java/com/inti/model/Destination.java
@@ -1,5 +1,49 @@
 package com.inti.model;
 
 public class Destination {
+	
+	private Long idDestination;
+	private Long longitude;
+	private Long latitude;
+	
+	public Destination() {
+		
+	}
 
+	public Destination(Long idDestination, Long longitude, Long latitude) {
+		this.idDestination = idDestination;
+		this.longitude = longitude;
+		this.latitude = latitude;
+	}
+
+	public Long getIdDestination() {
+		return idDestination;
+	}
+
+	public void setIdDestination(Long idDestination) {
+		this.idDestination = idDestination;
+	}
+
+	public Long getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Long longitude) {
+		this.longitude = longitude;
+	}
+
+	public Long getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Long latitude) {
+		this.latitude = latitude;
+	}
+
+	@Override
+	public String toString() {
+		return "Destination [idDestination=" + idDestination + ", longitude=" + longitude + ", latitude=" + latitude
+				+ "]";
+	}
+	
 }
