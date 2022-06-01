90888a519d774c28b83614d9928f7fd44f5e1596
root <root@localhost.localdomain>
2022-06-01 15:35:27 +0200

    Ajout classe Avis avec ManyToOne

---
+++ src/main/java/com/inti/model/Avis.java
@@ -0,0 +1,44 @@
+package com.inti.model;
+
+import javax.persistence.ManyToOne;
+
+public class Avis {
+	
+	private Long idAvis;
+	private String commentaire;
+	
+	@ManyToOne
+	private Hotel hotel;
+	
+	public Avis() {
+	
+	}
+
+	public Avis(Long idAvis, String commentaire) {
+		this.idAvis = idAvis;
+		this.commentaire = commentaire;
+	}
+
+	public Long getIdAvis() {
+		return idAvis;
+	}
+
+	public void setIdAvis(Long idAvis) {
+		this.idAvis = idAvis;
+	}
+
+	public String getCommentaire() {
+		return commentaire;
+	}
+
+	public void setCommentaire(String commentaire) {
+		this.commentaire = commentaire;
+	}
+
+	@Override
+	public String toString() {
+		return "Avis [idAvis=" + idAvis + ", commentaire=" + commentaire + "]";
+	}
+	
+	
+}
