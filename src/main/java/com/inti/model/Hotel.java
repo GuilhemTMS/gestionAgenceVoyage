b26d2d4263624f9acdcd0af98aab5fc8417c1acf
FlorianBousquet34 <florianbousquet86@gmail.com>
2022-06-01 15:14:08 +0200

    ajout classe Hotel

---
+++ src/main/java/com/inti/model/Hotel.java
@@ -0,0 +1,56 @@
+package com.inti.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Hotel implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idHotel;
+	private String nom;
+	private int nbEtoile;
+	
+	public Hotel() {
+		
+	}
+
+	public Hotel(String nom, int nbEtoile) {
+		this.nom = nom;
+		this.nbEtoile = nbEtoile;
+	}
+
+	public Long getIdHotel() {
+		return idHotel;
+	}
+
+	public void setIdHotel(Long idHotel) {
+		this.idHotel = idHotel;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getNbEtoile() {
+		return nbEtoile;
+	}
+
+	public void setNbEtoile(int nbEtoile) {
+		this.nbEtoile = nbEtoile;
+	}
+
+	@Override
+	public String toString() {
+		return "Hotel [idHotel=" + idHotel + ", nom=" + nom + ", nbEtoile=" + nbEtoile + "]";
+	}
+	
+}
